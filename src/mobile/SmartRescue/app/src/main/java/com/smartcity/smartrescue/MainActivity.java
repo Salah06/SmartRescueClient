1d23653a1c8738ccc04b29680e5e433307144775
Pascal Tung <tung.pascal@etu.unice.fr>
2016-10-23 09:54:55 +0200

    Get GPS coord

--- src/mobile/SmartRescue/app/src/main/java/com/smartcity/smartrescue/MainActivity.java
+++ src/mobile/SmartRescue/app/src/main/java/com/smartcity/smartrescue/MainActivity.java
@@ -1,7 +1,12 @@
 package com.smartcity.smartrescue;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import timber.log.Timber;
 
@@ -9,10 +14,28 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Timber.plant(new Timber.DebugTree());
         super.onCreate(savedInstanceState);
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
         setContentView(R.layout.activity_main);
 
-        Timber.d("Test");
+        if (ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  },
+                    LocationService.MY_PERMISSION_ACCESS_COURSE_LOCATION );
+        }
+
+        LocationService ls = LocationService.getLocationManager(this);
+        Location loc = ls.location;
+
+        if (null != loc) {
+            TextView gpsCoordView = (TextView) findViewById(R.id.gps_coord);
+            String str = "Lat : " +
+                    loc.getLatitude() +
+                    "Long : " +
+                    loc.getLongitude();
+
+            gpsCoordView.setText(str);
+        }
     }
 }
