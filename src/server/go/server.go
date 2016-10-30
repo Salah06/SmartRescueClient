7667dd9ca414c2f3cef13c54fb12eaa79bc19683
Simon PARIS <ps205947@etu.unice.fr>
2016-10-30 09:46:08 +0100

    link btw Go server and Firebase OK

--- src/server/go/server.go
+++ src/server/go/server.go
@@ -9,27 +9,36 @@
 package main
 
 import (
-    "net"
-    "bufio"
     "log"
     "fmt"
     "gopkg.in/zabawaba99/firego.v1"
 )
 
 
-// +slide
-func handleConnection(conn net.Conn) {
-    defer conn.Close()
+var f = firego.New("https://testrescue-d8b04.firebaseio.com/", nil)
 
-    reader := bufio.NewReader(conn)
-    for {
-        line,err := reader.ReadString('\n')
-        if err != nil {
-            log.Println(err)
-            return
-        }
-        fmt.Fprintf(conn, "You said => %s", line);
+func pushValue() {
+    v := "bar"
+    pushedFirego, err := f.Push(v)
+    if err != nil {
+        log.Fatal(err)
     }
+
+    var bar string
+    if err := pushedFirego.Value(&bar); err != nil {
+        log.Fatal(err)
+    }
+
+    // prints "https://my-firebase-app.firebaseIO.com/-JgvLHXszP4xS0AUN-nI: bar"
+    fmt.Printf("%s: %s\n", pushedFirego, bar)
+}
+
+func getValue() {
+    var v map[string]interface{}
+    if err := f.Value(&v); err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("%s\n", v)
 }
 
 func CheckError(err error) {
@@ -39,44 +48,7 @@ func CheckError(err error) {
 }
 
 func main() {
-    f := firego.New("https://smartrescue-6e8ce.firebaseio.com", client)
-    f.Auth("AIzaSyAtsc7v1IEUIPsD3dHkIev3B8ysHvunLd0")
-    f.Unauth()
-
-    var v map[string]interface{}
-    if err := f.Value(&v); err != nil {
-        log.Fatal(err)
-    
-    fmt.Printf("%s\n", v)
-
+    //f.Auth("null")
+    pushValue()
+    getValue()
 }
-
-    /*
-    ServerAddr,err := net.ResolveUDPAddr("udp",":2345")
-    CheckError(err)
-
-
-    ServerConn, err := net.ListenUDP("udp", ServerAddr)
-    defer ServerConn.Close()
-    CheckError(err)
-
-    buf := make([]byte, 1024)
-
-    for {
-        n,addr,err := ServerConn.ReadFromUDP(buf)
-        fmt.Println("Received ",string(buf[0:n]), " from ",addr)
-
-        if err != nil {
-            fmt.Println("Error: ",err)
-        } 
-        ServerConn.WriteToUDP([]byte("ack"), addr)
-    }
-}
-// ---
-// Local Variables:
-// compile-command: "go run *.go"
-// End:
-
-// pour tester : nc localhost 1234
-
-// on met go pour lancer une fonction en arriere plan (goroutine)
